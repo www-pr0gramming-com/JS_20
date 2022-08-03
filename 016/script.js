0351cb1a75420b2498087619cce94d535a649adb
c0rat1e5 <cccc0rat1e5@icloud.com>
2022-08-03 16:06:41 +0900

    push all files

---
+++ 016/script.js
@@ -0,0 +1,204 @@
+const addBtns = document.querySelectorAll(".add-btn:not(.solid)");
+const saveItemBtns = document.querySelectorAll(".solid");
+const addItemContainers = document.querySelectorAll(".add-container");
+const addItems = document.querySelectorAll(".add-item");
+// Item Lists
+const listColumns = document.querySelectorAll(".drag-item-list");
+const backlogList = document.getElementById("backlog-list");
+const progressList = document.getElementById("progress-list");
+const completeList = document.getElementById("complete-list");
+const onHoldList = document.getElementById("on-hold-list");
+
+// Items
+let updatedOnLoad = false;
+
+// Initialize Arrays
+let backlogListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
+let listArrays = [];
+
+// Drag Functionality
+let draggedItem;
+let dragging = false;
+let currentColumn;
+
+// Get Arrays from localStorage if available, set default values if not
+function getSavedColumns() {
+  if (localStorage.getItem("backlogItems")) {
+    backlogListArray = JSON.parse(localStorage.backlogItems);
+    progressListArray = JSON.parse(localStorage.progressItems);
+    completeListArray = JSON.parse(localStorage.completeItems);
+    onHoldListArray = JSON.parse(localStorage.onHoldItems);
+  } else {
+    backlogListArray = ["Release the course", "Sit back and relax"];
+    progressListArray = ["Work on projects", "Listen to music"];
+    completeListArray = ["Being cool", "Getting stuff done"];
+    onHoldListArray = ["Being uncool"];
+  }
+}
+
+// Set localStorage Arrays
+function updateSavedColumns() {
+  listArrays = [
+    backlogListArray,
+    progressListArray,
+    completeListArray,
+    onHoldListArray,
+  ];
+  const arrayNames = ["backlog", "progress", "complete", "onHold"];
+
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(
+      `${arrayName}Items`,
+      JSON.stringify(listArrays[index])
+    );
+  });
+}
+
+// Filter Array to remove empty values
+// function filterArray(array) {
+//   const filteredArray = array.filter((item) => item !== null);
+//   return filteredArray;
+// }
+
+// Create DOM Elements for each list item
+function createItemEl(columnEl, column, item, index) {
+  // List Item
+  const listEl = document.createElement("li");
+  listEl.classList.add("drag-item");
+  listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute("ondragstart", "drag(event)");
+  listEl.contentEditable = true;
+  listEl.id = `c${column}-${index}`;
+  listEl.setAttribute("onfocusout", `updateItem(${column}, ${index})`);
+  // Append
+  columnEl.appendChild(listEl);
+}
+
+// Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
+function updateDOM() {
+  // Check localStorage once
+  if (!updatedOnLoad) {
+    getSavedColumns();
+    updatedOnLoad = true;
+  }
+  // Backlog Column
+  backlogList.textContent = "";
+  backlogListArray.forEach((backlogItem, index) => {
+    createItemEl(backlogList, 0, backlogItem, index);
+  });
+  // Progress Column
+  progressList.textContent = "";
+  progressListArray.forEach((progressItem, index) => {
+    createItemEl(progressList, 1, progressItem, index);
+  });
+  // Complete Column
+  completeList.textContent = "";
+  completeListArray.forEach((completeItem, index) => {
+    createItemEl(completeList, 2, completeItem, index);
+  });
+  // On Hold Column
+  onHoldList.textContent = "";
+  onHoldListArray.forEach((onHoldItem, index) => {
+    createItemEl(onHoldList, 3, onHoldItem, index);
+  });
+
+  // Run getSavedColumns only once, Update Local Storage
+  updateSavedColumns();
+}
+
+// Update Item - Delete if necessary, or update Array value
+function updateItem(column, index) {
+  const selectedArray = listArrays[column];
+  const selectedColumnEl = listColumns[column].children;
+  if (!dragging) {
+    if (!selectedColumnEl[index].textContent) {
+      selectedArray.splice(index, 1);
+    } else {
+      selectedArray[index] = selectedColumnEl[index].textContent;
+    }
+    updateDOM();
+  }
+}
+
+// Add to Column List, Reset Textbox
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  if (itemText.trim() === "") return;
+
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = "";
+  updateDOM();
+}
+
+// Show Add Item Input Box
+function showInputBox(column) {
+  addBtns[column].style.visibility = "hidden";
+  saveItemBtns[column].style.display = "flex";
+  addItemContainers[column].style.display = "flex";
+  addItems[column].focus();
+}
+
+// Hide Item Input Box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = "visible";
+  saveItemBtns[column].style.display = "none";
+  addItemContainers[column].style.display = "none";
+  addToColumn(column);
+}
+
+// Allow arrays to reflect Drag and Drop items
+function rebuildArrays() {
+  backlogListArray = Array.from(backlogList.children).map(
+    (item) => item.textContent
+  );
+  progressListArray = Array.from(progressList.children).map(
+    (item) => item.textContent
+  );
+  completeListArray = Array.from(completeList.children).map(
+    (item) => item.textContent
+  );
+  onHoldListArray = Array.from(onHoldList.children).map(
+    (item) => item.textContent
+  );
+  updateDOM();
+}
+
+// When Item Starts Dragging
+function drag(e) {
+  draggedItem = e.target;
+  dragging = true;
+}
+
+// Column Allows for Item to Drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// When Item Enters Column Area
+function dragEnter(column) {
+  listColumns[column].classList.add("over");
+  currentColumn = column;
+}
+
+// Dropping Item in Column
+function drop(e) {
+  e.preventDefault();
+  // Remove Background Color/Padding
+  listColumns.forEach((column) => {
+    column.classList.remove("over");
+  });
+  // Add Item to Column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
+  // Dragging complete
+  dragging = false;
+  rebuildArrays();
+}
+
+// On Load
+updateDOM();
---
+++ 018/script.js
@@ -0,0 +1,39 @@
+const { body } = document;
+
+/** Memory Leak Testing */
+// let count = 0;
+// const ourMemory = {
+//   [count]: Array(10000).fill("*"),
+// };
+
+function changeBackground(number) {
+  /** Memory Leak Testing */
+  // count++;
+  // ourMemory[count] = Array(10000).fill("*");
+
+  // Check if background already showing
+  let previousBackground;
+  if (body.className) {
+    previousBackground = body.className;
+  }
+
+  // Reset background
+  body.className = "";
+  // If background already on, turn off, else turn on background
+  switch (number) {
+    case "1":
+      return previousBackground === "background-1"
+        ? false
+        : body.classList.add("background-1");
+    case "2":
+      return previousBackground === "background-2"
+        ? false
+        : body.classList.add("background-2");
+    case "3":
+      return previousBackground === "background-3"
+        ? false
+        : body.classList.add("background-3");
+    default:
+      break;
+  }
+}
